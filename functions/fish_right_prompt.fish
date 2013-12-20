10b209c9882c371afbc6b8f03f7b71dda5369a1e
Bruno Arueira <contato@brunoarueira.com>
2013-12-19 23:33:57 -0200

    Customized fish right prompt function

---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,14 @@
+function fish_right_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l red (set_color -o red)
+  set -l normal (set_color normal)
+
+  echo -n -s $cyan (prompt_pwd)
+
+  if test $last_status -ne 0
+    set_color red
+    printf ' %d' $last_status
+    set_color normal
+  end
+end
