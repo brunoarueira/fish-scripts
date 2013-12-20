1d05992312e5221730bdb1af51bf725ba0ddf724
Bruno Arueira <contato@brunoarueira.com>
2013-12-19 23:30:32 -0200

    Improved prompt with git dirty color status at branch

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,9 +1,21 @@
-set fish_git_dirty_color red
+function is_git_dirty
+  echo (git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function git_dirty_color
+  set -x dirty_color (set_color green)
+
+  if [ (is_git_dirty) ]
+    set -x dirty_color (set_color red)
+  end
+
+  echo $dirty_color
+end
 
 function parse_git_branch
   set -l branch (git branch 2>/dev/null | grep -e '^*' | sed -E 's/^\* (.+)$/(\1) /')
 
-  echo (set_color $fish_git_dirty_color)$branch(set_color red)
+  echo (git_dirty_color) $branch
 end
 
 function get_ruby_version
@@ -11,5 +23,5 @@ function get_ruby_version
 end
 
 function fish_prompt
-  printf '%s%s %s[%s]%s %s%s %s%s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color purple) (get_ruby_version) (set_color red) (parse_git_branch) (set_color normal)
+  printf '%s%s %s %s%s %s%s$ ' (set_color green) (whoami) (set_color purple) (get_ruby_version) (parse_git_branch) (set_color normal)
 end
