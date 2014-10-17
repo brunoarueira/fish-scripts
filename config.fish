24e7aec3b0b7e06773c04df68890ff530d8a2101
Bruno Arueira <contato@brunoarueira.com>
2014-10-17 09:51:19 -0300

    Remove rvm and added rbenv

--- config.fish
+++ config.fish
@@ -5,7 +5,13 @@ set default_path /usr/bin /usr/sbin /bin /sbin
 set homebrew /usr/local/bin /usr/local/sbin
 set heroku /usr/local/heroku/bin
 set npm /usr/local/share/npm/bin
-set -gx PATH $homebrew $heroku $npm $default_path
+set brew_rbenv "/usr/local/var/rbenv/shims"
+set -gx PATH $homebrew $heroku $npm $brew_rbenv $default_path
+
+### Ruby (rbenv) ###
+set -gx RBENV_ROOT /usr/local/var/rbenv
+
+rbenv rehash >/dev/null ^&1
 
 # Default editor
 set -U EDITOR vim
@@ -13,9 +19,6 @@ set -U EDITOR vim
 # Ruby env vars
 set RUBY_GC_MALLOC_LIMIT 60000000
 set RUBY_FREE_MIN 200000
-
-# hack to load rvm custom with fish
-rvm > /dev/null
 
 #aliases
 alias rake='bundle exec rake'
