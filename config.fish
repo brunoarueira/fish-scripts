594fd251dfa275d3505dedbb924c4a2b3564b819
Bruno Arueira <contato@brunoarueira.com>
2015-01-02 14:27:48 -0200

    Added Postgres.app bin folder into PATH

--- config.fish
+++ config.fish
@@ -1,13 +1,23 @@
 set fish_greeting ""
 
+# Choose the last version postgres installed by Postgres.app
+set postgresapp_base_path /Applications/Postgres.app/Contents/Versions
+
+function choose_pg_version
+  ls $postgresapp_base_path | sort -k 2 -nr | head -n 1
+end
+
+set -q PG_VERSION; or set PG_VERSION (choose_pg_version)
+
 ### PATH ###
-set default_path /usr/bin /usr/sbin /bin /sbin
+set postgresapp $postgresapp_base_path/$PG_VERSION/bin
 set homebrew /usr/local/bin /usr/local/sbin
 set heroku /usr/local/heroku/bin
 set npm /usr/local/share/npm/bin
 set rbenv_bin $HOME/.rbenv/bin
 set rbenv_shims $HOME/.rbenv/shims
-set -gx PATH $homebrew $heroku $npm $rbenv_bin $rbenv_shims $default_path
+set default_path /usr/bin /usr/sbin /bin /sbin
+set -gx PATH $postgresapp $homebrew $heroku $npm $rbenv_bin $rbenv_shims $default_path
 
 ### Ruby (rbenv) ###
 rbenv rehash >/dev/null ^&1
