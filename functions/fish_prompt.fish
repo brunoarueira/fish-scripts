fa86b90eb28b5fa23b62396ec9480ee8d1a71831
Bruno Arueira <contato@brunoarueira.com>
2015-10-14 11:52:51 -0300

    Modify the prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,27 +1,147 @@
-function is_git_dirty
-  echo (git status -s --ignore-submodules=dirty ^/dev/null)
+function __parse_current_folder -d "Replace '/Users/$USER' by '~'"
+  pwd | sed "s/^\/Users\/$USER/~/"
 end
 
-function git_dirty_color
+function __git_dirty_color
   set -x dirty_color (set_color green)
+  set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
 
-  if [ (is_git_dirty) ]
+  if test -n "$is_git_dirty"
     set -x dirty_color (set_color red)
   end
 
   echo $dirty_color
 end
 
-function parse_git_branch
-  set -l branch (git branch 2>/dev/null | grep -e '^*' | sed -E 's/^\* (.+)$/(\1) /')
+function __parse_git_branch -d "Parse current Git branch name"
+  set -l branch (git symbolic-ref HEAD | sed -e "s/^refs\/heads\///")
 
-  echo (git_dirty_color) $branch
+  echo (__git_dirty_color) $branch
 end
 
-function get_ruby_version
+function __format_time -d "Format milliseconds to a human readable format"
+  set -l millseconds $argv[1]
+  set -l seconds (math "$millseconds / 1000 % 60")
+  set -l minutes (math "$millseconds / 60000 % 60")
+  set -l hours (math "$millseconds / 3600000 % 24")
+  set -l days (math "$millseconds / 86400000")
+  set -l time ""
+  set -l threshold 5
+
+  if test $days -gt 0
+    set time (command printf "$time%sd " $days)
+  end
+
+  if test $hours -gt 0
+    set time (command printf "$time%sh " $hours)
+  end
+
+  if test $minutes -gt 0
+    set time (command printf "$time%sm " $minutes)
+  end
+
+  if test $seconds -gt $threshold
+    set time (command printf "$time%ss" $seconds)
+  end
+
+  echo -e $time
+end
+
+function __get_ruby_version
   echo (ruby --version | cut -d' ' -f2)
 end
 
 function fish_prompt
-  printf '%s%s %s %s%s %s%s$ ' (set_color green) (whoami) (set_color purple) (get_ruby_version) (parse_git_branch) (set_color normal)
+  set -l exit_code $status
+
+  # Symbols
+
+  set -l symbol_prompt "❯"
+  set -l symbol_git_down_arrow "⇣"
+  set -l symbol_git_up_arrow "⇡"
+
+  # Colors
+
+  set -l color_blue (set_color blue)
+  set -l color_green (set_color green)
+  set -l color_normal (set_color normal)
+  set -l color_purple (set_color purple)
+  set -l color_red (set_color red)
+  set -l color_yellow (set_color yellow)
+
+  # Template
+
+  set -l current_folder (__parse_current_folder)
+  set -l command_duration ""
+  set -l prompt ""
+  set -l git_arrows ""
+  set -l whoami (whoami)
+
+  # Format current folder on prompt output
+  set prompt $prompt "\n$color_green$whoami $color_blue$current_folder$color_normal "
+
+  # Handle previous command exit code
+  if test $exit_code -ne 0
+    # Symbol color is red when previous command fails
+    set color_symbol $color_red
+
+    # Prompt failed command execution duration
+    set command_duration (__format_time $CMD_DURATION)
+
+    set prompt $prompt "$color_yellow$command_duration$color_normal "
+  end
+
+  # Exit with code 1 if git is not available
+  if not command -s git >/dev/null
+    return 1
+  end
+
+  # Check if is on a Git repository
+  set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
+
+  set current_ruby_version (__get_ruby_version)
+
+  set prompt $prompt "$color_purple$current_ruby_version$color_normal"
+
+  if test -n "$is_git_repository"
+    set git_branch_name (__parse_git_branch)
+
+    # Check if there is an upstream configured
+    set -l has_upstream (command git rev-parse --abbrev-ref @'{u}' ^/dev/null)
+
+    if test -n "$has_upstream"
+      set -l git_status (command git rev-list --left-right --count HEAD...@'{u}' | sed 's/[[:blank:]]/ /' ^/dev/null)
+
+      # Resolve Git arrows by treating `git_status` as an array
+      set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
+      set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
+
+      # If arrow is not "0", it means it's dirty
+      if test $git_arrow_left -ne "0"
+        set git_arrows $symbol_git_up_arrow
+      end
+
+      if test $git_arrow_right -ne "0"
+        set git_arrows $git_arrows$symbol_git_down_arrow
+      end
+    end
+
+    # Format Git prompt output
+    set prompt $prompt "$color_gray$git_branch_name$color_normal\t$color_cyan$git_arrows$color_normal"
+  end
+
+  set prompt $prompt "\n$color_symbol$symbol_prompt$color_normal "
+
+  echo -e -s $prompt
+
+  # printf '%s%s %s %s%s %s%s$ ' ($color_green) (whoami) ($color_purple) (__get_ruby_version) (__parse_git_branch) ($color_normal)
+end
+
+# Set title to current folder and shell name
+function fish_title
+  echo (__parse_current_folder) – $_
+end
+
+# Removes right prompt
+function fish_right_prompt
 end
