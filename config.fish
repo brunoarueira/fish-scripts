22d2b9c13bda5d133ed10f9ee2d88d4143925aef
Bruno Arueira <contato@brunoarueira.com>
2016-11-16 08:37:32 -0200

    Removed the postgres var which integrates the Postgres app with console
    I returned back to homebrew postgres.

--- config.fish
+++ config.fish
@@ -8,15 +8,6 @@ set -gx LC_MESSAGES 'en_US.UTF-8'
 ### Go ###
 set -gx GOPATH /Users/bruno_arueira/trabalho/go
 
-# Choose the last version postgres installed by Postgres.app
-set postgresapp_base_path /Applications/Postgres.app/Contents/Versions
-
-function choose_pg_version
-  ls $postgresapp_base_path | sort -k 2 -nr | head -n 1
-end
-
-set -q PG_VERSION; or set PG_VERSION (choose_pg_version)
-
 # Sets java home
 set JAVA_HOME "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands"
 
@@ -24,7 +15,6 @@ set JAVA_HOME "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Comm
 set -gx ANDROID_HOME /usr/local/opt/android-sdk
 
 ### PATH ###
-set postgresapp $postgresapp_base_path/$PG_VERSION/bin
 set homebrew /usr/local/bin /usr/local/sbin
 set heroku /usr/local/heroku/bin
 set npm /usr/local/share/npm/bin
@@ -33,7 +23,7 @@ set rbenv_bin $HOME/.rbenv/bin
 set rbenv_shims $HOME/.rbenv/shims
 set gopathbin $GOPATH/bin
 set default_path /usr/bin /usr/sbin /bin /sbin
-set -gx PATH $postgresapp $homebrew $heroku $npm $node_modules $rbenv_bin $rbenv_shims $gopathbin $default_path
+set -gx PATH $homebrew $heroku $npm $node_modules $rbenv_bin $rbenv_shims $gopathbin $default_path
 
 ### Ruby (rbenv) ###
 rbenv rehash >/dev/null ^&1
