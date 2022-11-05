7c329a68c75d036c162e96d0d5ab6b3f232847d4
Bruno Arueira <contato@brunoarueira.com>
2022-11-05 18:08:57 -0300

    Improves k8s helper functions to set namespace correctly

--- functions/klogs.fish
+++ functions/klogs.fish
@@ -1,15 +1,15 @@
 function klogs --argument-names namespace -d 'Access logs through kubectl from pods on the specified namespace'
-  if test -z "$namespace"
-    set namespace_command "-n $namespace"
+  if ! test -n "$namespace"
+    set namespace "default"
   end
 
   function __list_pods --inherit-variable namespace
-    kubectl $namespace_command get pods --no-headers
+    kubectl -n $namespace get pods --no-headers
   end
 
   set pod (__list_pods | fzf | awk '{print $1}')
 
   if test -n "$pod"
-    kubectl $namespace_command logs -f $pod
+    kubectl -n $namespace logs -f $pod
   end
 end
