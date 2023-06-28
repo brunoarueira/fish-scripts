5066a58a3b9da6943344ef90704db783fa2041a9
Bruno Arueira <contato@brunoarueira.com>
2023-06-28 17:18:20 -0300

    Improves ssm function to list by launch time

--- functions/ssm.fish
+++ functions/ssm.fish
@@ -5,12 +5,12 @@ function ssm --argument-names profile -d 'Connect through aws ssm to instances o
 
   function __list_instances --inherit-variable profile
     aws ec2 describe-instances \
-      --filters Name=instance-state-name,Values=running --query 'reverse(Reservations[*].Instances[*].[InstanceId,PrivateIpAddress,LaunchTime,Tags[?Key==`Name`]|[0].Value][] | sort_by(@, &[2]))' \
+      --filters Name=instance-state-name,Values=running --query 'reverse(Reservations[*].Instances[*].[LaunchTime,PrivateIpAddress,InstanceId,Tags[?Key==`Name`]|[0].Value][] | sort_by(@, &[0]))' \
       --profile $profile \
       --output text
   end
 
-  set instance (__list_instances | fzf | cut -f 1)
+  set instance (__list_instances | fzf | cut -f 3)
 
   if test -n "$instance"
     aws ssm start-session --target $instance --profile $profile
