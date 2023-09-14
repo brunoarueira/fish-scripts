15fb8076788c5cd96deb63497357e439f807d004
Bruno Arueira <contato@brunoarueira.com>
2023-09-14 11:50:06 -0300

    Improves kexec to show the current context and avoid trouble

--- functions/kexec.fish
+++ functions/kexec.fish
@@ -3,11 +3,17 @@ function kexec --argument-names namespace -d 'Connect through kubectl to pods on
     set namespace "default"
   end
 
+  set context (kubectl config current-context)
+
   function __list_pods --inherit-variable namespace
     kubectl -n $namespace get pods --no-headers
   end
 
-  set pod (__list_pods | fzf | awk '{print $1}')
+  set pod (
+  	__list_pods |
+	fzf --header="Current context: $context" --header-lines=0 --color "header:blue" |
+	awk '{print $1}'
+	)
 
   if test -n "$pod"
     kubectl -n $namespace exec -it $pod -- sh
