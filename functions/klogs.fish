6418d24741cc750585d683354707999e60c9e63a
Bruno Arueira <contato@brunoarueira.com>
2024-01-16 14:26:41 -0300

    feat: Adds a way to list init containers from pods and show their logs

--- functions/klogs.fish
+++ functions/klogs.fish
@@ -1,4 +1,4 @@
-function klogs --argument-names namespace -d 'Access logs through kubectl from pods on the specified namespace'
+function klogs --argument-names namespace --argument-names init_containers -d 'Access logs through kubectl from pods on the specified namespace'
   if ! test -n "$namespace"
     set namespace "default"
   end
@@ -9,7 +9,17 @@ function klogs --argument-names namespace -d 'Access logs through kubectl from p
 
   set pod (__list_pods | fzf | awk '{print $1}')
 
-  if test -n "$pod"
+  if test -n "$init_containers"
+    function __list_init_containers --inherit-variable namespace --inherit-variable pod
+      kubectl -n $namespace get pods $pod -o jsonpath="{.spec.initContainers[*].name}" | tr ' ' '\n'
+    end
+
+    set selected_init_container (__list_init_containers | fzf | awk '{print $1}')
+  end
+
+  if test -n "$pod" && ! test -n "$selected_init_container"
     kubectl -n $namespace logs -f $pod
+  else if test -n "$pod" && test -n "$selected_init_container"
+    kubectl -n $namespace logs -f $pod -c $selected_init_container
   end
 end
