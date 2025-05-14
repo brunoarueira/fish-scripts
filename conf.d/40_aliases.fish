cd1ce066167e9df759f49f2f07f01cd500b29dce
Bruno Arueira <contato@brunoarueira.com>
2025-05-14 14:19:10 -0300

    feat: add alias g for git

--- conf.d/40_aliases.fish
+++ conf.d/40_aliases.fish
@@ -51,3 +51,5 @@ alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ and print $1'"
 
 # View HTTP traffic
 alias sniff="sudo ngrep -d '$argv' -t '^(GET|POST) ' 'tcp and port 80'"
+
+alias g='git'
